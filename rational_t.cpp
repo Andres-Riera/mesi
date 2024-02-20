af38001d0fbf9a93080fa8ce2d749b4e55d69520
Andrés David Riera Rivera <alu0101618654@ull.edu.es>
2024-02-20 12:33:05 +0000

    Add files via upload

--- rational_t.cpp
+++ rational_t.cpp
@@ -1,15 +1,14 @@
-/* AUTOR: Andrés David Riera Rivera
-*  FECHA: 6 de febrero de 2024
-*  EMAIL: alu0101618654@ull.edu.es
-*  VERSION: 1.0
-*  ASIGNATURA: Algoritmos y Estructuras de Datos
-*  PRÁCTICA Nº: 1
-*  COMENTARIOS: se indican entre [] las pautas de estilo aplicadas de
-*               "C++ Programming Style Guidelines"
-*               https://geosoft.no/development/cppstyle.html
-*/
+// AUTOR: Andrés David Riera Rivera
+// FECHA: 20-02-24
+// EMAIL: alu0101618654@ull.edu.es
+// VERSION: 1.0
+// ASIGNATURA: Algoritmos y Estructuras de Datos
+// PRÁCTICA Nº: 2
+// COMENTARIOS: se indican entre [] las pautas de estilo aplicadas de
+//              "C++ Programming Style Guidelines"
+//              https://geosoft.no/development/cppstyle.html
 
-// Pauta de estilo [92]: comentarios multilínea usando solo "//"
+// pauta de estilo [92]: comentarios multilínea usando solo "//"
 
 #include "rational_t.hpp"
 
@@ -17,64 +16,83 @@ rational_t::rational_t(const int n, const int d) {
   assert(d != 0);
   num_ = n, den_ = d;
 }
-// Pauta de estilo [87]: 3 líneas de separación entre métodos
-// Pauta de estilo [83]: tipo retornado en línea anterior al método
-int rational_t::get_num() const {
+// pauta de estilo [87]: 3 líneas de separación entre métodos
+// pauta de estilo [83]: tipo retornado en línea anterior al método
+inline int rational_t::get_num() const {
   return num_;
 }
 
-int rational_t::get_den() const {
+inline int rational_t::get_den() const {
   return den_;
 }
-
+  
 void rational_t::set_num(const int n) {
   num_ = n;
 }
-
+  
 void rational_t::set_den(const int d) {
   assert(d != 0);
   den_ = d;
 }
 
-double rational_t::value() const { 
+inline double rational_t::value() const { 
   return double(get_num()) / get_den();
 }
 
-// Comparaciones
-// Verifica si los racionales dados son iguales
-bool rational_t::is_equal(const rational_t& r, const double precision) const {
-  return (fabs(value() - r.value()) < precision);
+rational_t rational_t::opposite() const { 
+  return rational_t((-1)*get_num(), get_den());
 }
 
-// Verifica si el racional dado es mayor que el que llama al método
+rational_t rational_t::reciprocal() const { 
+  return rational_t(get_den(), get_num());
+}
+
+// comparaciones
+bool rational_t::is_equal(const rational_t& r, const double precision) const { 
+  return fabs(value() - r.value()) < precision;
+}
+
 bool rational_t::is_greater(const rational_t& r, const double precision) const {
-  return (r.value() - value() > precision);
+  return (value() - r.value()) > precision;
 }
 
-// Verifica si el racional dado es menor que el que llama al método
 bool rational_t::is_less(const rational_t& r, const double precision) const {
-  return (value() - r.value() > precision);
+  return r.is_greater(*this, precision);
 }
 
-// Operaciones
-// Suma de racionales
-rational_t rational_t::add(const rational_t& r) {
-  return rational_t(num_ * r.get_den() + r.get_num() * den_, den_ * r.get_den());
+// operaciones
+rational_t rational_t::add(const rational_t& r) const {
+  return rational_t(get_num() * r.get_den() + get_den() * r.get_num(), 
+                    get_den() * r.get_den());
 }
 
-// Resta de racionales
-rational_t rational_t::substract(const rational_t& r) {
-  return rational_t(num_ * r.get_den() - r.get_num() * den_, den_ * r.get_den());
+rational_t rational_t::substract(const rational_t& r) const {
+  return add(r.opposite());
 }
 
-// Multiplicación de racionales
-rational_t rational_t::multiply(const rational_t& r) {
-  return rational_t(num_ * r.get_num(), den_ * r.get_den());
+rational_t rational_t::multiply(const rational_t& r) const {
+  return rational_t(get_num() * r.get_num(), get_den() * r.get_den());
 }
 
-// División de racionales
-rational_t rational_t::divide(const rational_t& r) {
-  return rational_t(num_ * r.get_den(), den_ * r.get_num());
+rational_t rational_t::divide(const rational_t& r) const {
+  return multiply(r.reciprocal());
+}
+
+// FASE I: operadores
+rational_t operator+(const rational_t& a, const rational_t& b) {
+  return rational_t{a.get_num() * b.get_den() + a.get_den() * b.get_num(), a.get_den() * b.get_den()};
+}
+
+rational_t operator-(const rational_t& a, const rational_t& b) {
+  return rational_t{a.get_num() * b.get_den() - a.get_den() * b.get_num(), a.get_den() * b.get_den()};
+}
+
+rational_t operator*(const rational_t& a, const rational_t& b) {
+  return rational_t{a.get_num() * b.get_num(), a.get_den() * b.get_den()};
+}
+
+rational_t operator/(const rational_t& a, const rational_t& b) {
+  return rational_t{a.get_num() * b.get_den(), a.get_den() * b.get_num()};
 }
 
 // E/S
@@ -83,9 +101,16 @@ void rational_t::write(ostream& os) const {
 }
 
 void rational_t::read(istream& is) {
-  cout << "Numerador? ";
-  is >> num_;
-  cout << "Denominador? ";
-  is >> den_;
+  is >> num_ >> den_;
   assert(den_ != 0);
+}
+
+ostream& operator<<(ostream& os, const rational_t& r) {
+  r.write(os);
+  return os;
+}
+
+istream& operator>>(istream& is, rational_t& r) {
+  r.read(is);
+  return is;
 }
--- rational_t.hpp
+++ rational_t.hpp
@@ -1,13 +1,12 @@
-/* AUTOR: Andrés David Riera Rivera
-*  FECHA: 6 de febrero de 2024
-*  EMAIL: alu0101618654@ull.edu.es
-*  VERSION: 1.0
-*  ASIGNATURA: Algoritmos y Estructuras de Datos
-*  PRÁCTICA Nº: 1
-*  COMENTARIOS: se indican entre [] las pautas de estilo aplicadas de
-*               "C++ Programming Style Guidelines"
-*               https://geosoft.no/development/cppstyle.html
-*/
+// AUTOR: Andrés David Riera Rivera
+// FECHA: 20-02-24
+// EMAIL: alu0101618654@ull.edu.es
+// VERSION: 1.0
+// ASIGNATURA: Algoritmos y Estructuras de Datos
+// PRÁCTICA Nº: 2
+// COMENTARIOS: se indican entre [] las pautas de estilo aplicadas de
+//              "C++ Programming Style Guidelines"
+//              https://geosoft.no/development/cppstyle.html
 
 // pauta de estilo [92]: comentarios multilínea usando solo "//"
 
@@ -18,7 +17,7 @@
 #include <cmath>
 
 // pauta de estilo [5]
-# define EPSILON 1e-6
+# define kEPSILON 1e-6
 
 using namespace std;
 
@@ -29,6 +28,7 @@ public:
   ~rational_t() {}
   
   // pauta de estilo [71]: indentación a 2 espacios
+  
   // getters
   int get_num() const;
   int get_den() const;
@@ -38,22 +38,34 @@ public:
   void set_den(const int);
 
   double value(void) const;
+  rational_t opposite(void) const;
+  rational_t reciprocal(void) const;
 
-  // FASE II
-  bool is_equal(const rational_t&, const double precision = EPSILON) const;
-  bool is_greater(const rational_t&, const double precision = EPSILON) const;
-  bool is_less(const rational_t&, const double precision = EPSILON) const;
-  
-  // FASE III
-  rational_t add(const rational_t&);
-  rational_t substract(const rational_t&);
-  rational_t multiply(const rational_t&);
-  rational_t divide(const rational_t&);
-  
-  void write(ostream& = cout) const;
-  void read(istream& = cin);
+  bool is_equal(const rational_t&, const double precision = kEPSILON) const;
+  bool is_greater(const rational_t&, const double precision = kEPSILON) const;
+  bool is_less(const rational_t&, const double precision = kEPSILON) const;
+
+  rational_t add(const rational_t&) const;
+  rational_t substract(const rational_t&) const;
+  rational_t multiply(const rational_t&) const;
+  rational_t divide(const rational_t&) const;
+
+  void write(ostream& os = cout) const;
+  void read(istream& is = cin);
   
 private:
   // pauta de estilo [11]: nombre de los atributos seguido de "_"
   int num_, den_;
 };
+
+
+// sobrecarga de los operadores de E/S
+ostream& operator<<(ostream& os, const rational_t&);
+istream& operator>>(istream& is, rational_t&);
+
+// FASE I: operadores
+rational_t operator+(const rational_t&, const rational_t&);
+rational_t operator-(const rational_t&, const rational_t&);
+rational_t operator*(const rational_t&, const rational_t&);
+rational_t operator/(const rational_t&, const rational_t&);
+
