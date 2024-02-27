76c0ca3b45177be3c5ea1fb8ed089f3daef06dd4
Andrés David Riera Rivera <alu0101618654@ull.edu.es>
2024-02-27 10:13:55 +0000

    Update numero-e.s

--- PC/practica1/numero-e.s
+++ PC/practica1/numero-e.s
@@ -1,32 +1,89 @@
-#include <iostream>
+	.data
 
+titulo:		.asciiz "\nPR1: Principio de computadores.\n"
+pet:		.asciiz "\nIntroduzca maximo error permitido: "
+caderr:		.asciiz "\nError: el dato introducido debe cumplir 0.00001 <= dato < 1\n"
+cade:		.asciiz "\nNumero e: "
+cadnt:		.asciiz "\nNumero de terminos: "
+cadfin:		.asciiz "\nPR1: Fin del programa.\n"
+min_error:	.double 0.00001
+max_error:	.double 1.0
+uno:		.double 1.0
 
-#int main() {
- #   std::cout << "PR1: Principio de computadores.\n";
-  #  std::cout << "\n";
-   # double error;
-    #std::cout << "Introduzca el máximo error permitido: ";
-#    std::cin >> error;
- #   std::cout << "\n";
-  #  while (error < 0.00001 || error >= 1) {
-   #         std::cout << "Error: el dato introducido debe cumplir 0,00001 <= dato < 1\n";
-    #        std::cout << "\n";
-     #       std::cout << "Introduzca el máximo error permitido: ";
-      #      std::cin >> error;
-       #     std::cout << "\n";
-#    }
- #   double e{1.0};
-  #  double f{1.0};
-   # double ultimo{1.0};
-    #double terminos{1.0};
-    #while (ultimo > error) {
-     #   f = f * (terminos);
-      #  ultimo = 1.0 / f;
-       # e += ultimo;
-        #terminos += 1.0;
-#    }
- #   std::cout << "Numero e: " << e << "\n";
-  #  std::cout << "Numero de terminos: " << terminos << "\n";
-   # std::cout << "PR1: Fin del programa.\n";
-    #return 0;
-#}
+	.text
+
+main:
+
+	li $v0, 4
+	la $a0, titulo
+	syscall
+	li $v0, 4
+	la $a0, pet
+	syscall
+	li $v0, 6
+	syscall
+	mov.d $f2, $f0 # Maximo error permitido
+
+while_condicion1:
+
+	l.d $f4, min_error
+	l.d $f6, max_error
+	c.lt.d $f2, $f4
+	bc1t while1
+	c.lt.d $f6, $f2
+	bc1t while1
+	b while_fin1
+
+while1:
+
+	li $v0, 4
+	la $a0, caderr
+	syscall
+	li $v0, 4
+	la $a0, pet
+	syscall
+	li $v0, 6
+	syscall
+	mov.d $f2, $f0
+	b while_condicion1
+
+while_fin1:
+
+	l.d $f8, uno
+	mov.d $f10, $f8 # Numero e
+	mov.d $f12, $f8 # Factorial
+	mov.d $f14, $f8 # Ultimo termino
+	mov.d $f16, $f8 # Numero de terminos 
+
+while_condicion2:
+
+	c.lt.d $f2, $f14
+	bc1f while_fin2
+
+while2:
+
+	mul.d $f12, $f12, $f16
+	div.d $f14, $f8, $f12
+	add.d $f10, $f10, $f14
+	add.d $f16, $f16, $f8
+	j while_condicion2
+
+while_fin2:
+
+	li $v0, 4
+	la $a0, cade
+	syscall
+	li $v0, 2
+	mov.d $f12, $f10
+	syscall
+	li $v0, 4
+	la $a0, cadnt
+	syscall
+	li $v0, 2
+	mov.d $f12, $f16
+	syscall
+	li $v0, 4
+	la $a0, cadfin
+	syscall
+	li $v0, 10
+	syscall
